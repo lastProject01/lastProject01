7778402c6446bb451e1463f81a7953955b3fc3d8
usedcoding <ja14759@gmail.com>
2024-02-13 09:42:51 +0900

    init

---
+++ src/main/java/com/example/lastProject01/LastProject01Application.java
@@ -0,0 +1,13 @@
+package com.example.lastProject01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastProject01Application.class, args);
+	}
+
+}
