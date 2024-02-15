cebfb2d4ccc828b93e6326c7a6fb2cb6706626c7
jinyun-J <ppp6289@naver.com>
2024-02-15 13:15:16 +0900

    리뷰 기능 구현 시작

--- src/main/java/com/project/Enovel/domain/review/controller/ReviewController.java
+++ src/main/java/com/project/Enovel/domain/review/controller/ReviewController.java
@@ -1,4 +1,27 @@
 package com.project.Enovel.domain.review.controller;
 
+import com.project.Enovel.domain.review.entity.Review;
+import com.project.Enovel.domain.review.service.ReviewService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+@Controller
+@RequestMapping("/review")
+@RequiredArgsConstructor
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping("/create")
+    public String createReview(@RequestParam("memberId") Long memberId, @RequestParam("orderItemId") Long orderItemId, @RequestParam("content") String content) {
+
+        reviewService.create(memberId, orderItemId, content);
+        return "redirect:/reviewDetail";
+    }
 }
--- src/main/java/com/project/Enovel/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/project/Enovel/domain/review/repository/ReviewRepository.java
@@ -1,4 +1,9 @@
 package com.project.Enovel.domain.review.repository;
 
-public interface ReviewRepository {
+import com.project.Enovel.domain.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository <Review, Long>{
 }
