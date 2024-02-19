bcab4d96f7572ad1d2f1bb96aab3bd229b9fe44f
jinyun-J <ppp6289@naver.com>
2024-02-19 13:48:39 +0900

    오더 ã엔티티 수정중

--- src/main/java/com/project/Enovel/domain/order/entity/OrderItem.java
+++ src/main/java/com/project/Enovel/domain/order/entity/OrderItem.java
@@ -1,4 +1,11 @@
 package com.project.Enovel.domain.order.entity;
 
-public class OrderItem {
+import com.project.Enovel.global.base.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class OrderItem extends BaseEntity {
+    @ManyToOne
+    private Orders order;
 }
--- src/main/java/com/project/Enovel/domain/order/entity/Orders.java
+++ src/main/java/com/project/Enovel/domain/order/entity/Orders.java
@@ -1,8 +1,5 @@
 package com.project.Enovel.domain.order.entity;
 
-<<<<<<< HEAD
-public class Orders {
-=======
 import com.project.Enovel.domain.member.entity.Member;
 import com.project.Enovel.global.base.BaseEntity;
 import jakarta.persistence.Entity;
@@ -14,10 +11,8 @@ import lombok.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
 import static jakarta.persistence.CascadeType.ALL;
 import static lombok.AccessLevel.PROTECTED;
-
 @Entity
 @Builder
 @AllArgsConstructor(access = PROTECTED)
@@ -38,5 +33,5 @@ public class Orders extends BaseEntity {
     private LocalDateTime payDate; // 결제일
     private LocalDateTime cancelDate; // 취소일
     private LocalDateTime refundDate; // 환불일
->>>>>>> beec0018de7e596215416df72e4d0aa138a41cfe
+
 }
