01b4de3bfc8ef6a14014e512e8a5e47b82ab7956
usedcoding <ja14759@gmail.com>
2024-02-20 10:50:17 +0900

    2nd

--- src/main/java/com/project/Enovel/domain/order/entity/OrderItem.java
+++
@@ -1,8 +0,0 @@
-//package com.project.Enovel.domain.order.entity;
-//
-//import com.project.Enovel.global.base.BaseEntity;
-//import jakarta.persistence.Entity;
-//
-//@Entity
-//public class OrderItem extends BaseEntity {
-//}
