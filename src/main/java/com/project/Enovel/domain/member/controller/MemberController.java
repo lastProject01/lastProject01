40f3045e5083b1d3b1f24e214a9de86f6bd37fa8
sbk283 <sbk283@naver.com>
2024-02-21 13:24:43 +0900

    마이페이지 접근 시 패스워드 확인 후 일치시에 접근

--- src/main/java/com/project/Enovel/domain/member/controller/MemberController.java
+++ src/main/java/com/project/Enovel/domain/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.project.Enovel.domain.member.controller;
 
 import com.project.Enovel.domain.member.entity.Member;
+import com.project.Enovel.domain.member.form.MemberCheckedPwForm;
 import com.project.Enovel.domain.member.form.MemberCreateForm;
 import com.project.Enovel.domain.member.form.MemberMyPageForm;
 import com.project.Enovel.domain.member.form.MemberPasswordForm;
@@ -159,24 +160,24 @@ public class MemberController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/checkedPw")
-    public String checkedPw() {
+    public String checkedPw(MemberCheckedPwForm memberCheckedPwForm) {
         return "member/checked_pw";
     }
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/checkedPw")
-    public String checkedPw(@Valid MemberPasswordForm memberPasswordForm, BindingResult bindingResult, Principal principal, Model model) {
+    public String checkedPw(@Valid MemberCheckedPwForm memberCheckedPwForm, BindingResult bindingResult, Principal principal, Model model) {
         String username = principal.getName();
         Member member = memberService.getMember(username);
 
         // 기존 비밀번호 확인
-        if (!passwordEncoder.matches(memberPasswordForm.getConfirmPassword(), member.getPassword())) {
+        if (!passwordEncoder.matches(memberCheckedPwForm.getCheckedPassword(), member.getPassword())) {
             bindingResult.rejectValue("currentPassword", "passwordInCorrect", "비밀번호가 일치하지 않습니다.");
         }
 
         if (bindingResult.hasErrors()) {
             // 모델에 memberPasswordForm 추가
-            model.addAttribute("memberPasswordForm", memberPasswordForm);
+            model.addAttribute("memberCheckedPwForm", memberCheckedPwForm);
             return "member/checked_pw";
         }
 
