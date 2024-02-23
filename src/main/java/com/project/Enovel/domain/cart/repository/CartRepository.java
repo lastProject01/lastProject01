7475231100ec3aa5a7b3bc4151069ac5f3c2219d
usedcoding <ja14759@gmail.com>
2024-02-23 10:43:06 +0900

    cart error clear

--- src/main/java/com/project/Enovel/domain/cart/repository/CartRepository.java
+++ src/main/java/com/project/Enovel/domain/cart/repository/CartRepository.java
@@ -6,10 +6,14 @@ import com.project.Enovel.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     void deleteByProductAndMember(Product product, Member member);
 
     Cart getCartItemByProductAndMember(Product product, Member member);
+
+    Cart findByProduct(Product product);
 }
