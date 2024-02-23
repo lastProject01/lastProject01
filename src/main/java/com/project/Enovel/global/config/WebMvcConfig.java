675121dde6f5e81d4375870b1c54d99a41d4936c
usedcoding <ja14759@gmail.com>
2024-02-23 13:24:05 +0900

    picture

--- src/main/java/com/project/Enovel/global/config/WebMvcConfig.java
+++ src/main/java/com/project/Enovel/global/config/WebMvcConfig.java
@@ -7,12 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Value("${custom.fileDirPath}")
-    private String fileDirPath;
-
-    @Override
-    public void addResourceHandlers (ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/file/**")
-                .addResourceLocations("file:///" + fileDirPath + "/");
-    }
+//    @Value("${custom.fileDirPath}")
+//    private String fileDirPath;
+//
+//    @Override
+//    public void addResourceHandlers (ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/file/**")
+//                .addResourceLocations("file:///" + fileDirPath + "/");
+//    }
 }
