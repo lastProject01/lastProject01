349a43c02cc931826909547ad7304494a396a108
sbk283 <sbk283@naver.com>
2024-02-25 18:11:15 +0900

    멤버ajax 구현시작

--- src/main/java/com/project/Enovel/domain/member/controller/MemberController.java
+++ src/main/java/com/project/Enovel/domain/member/controller/MemberController.java
@@ -16,10 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
