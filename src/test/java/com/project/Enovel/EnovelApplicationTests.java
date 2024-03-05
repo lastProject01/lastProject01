f6947f3f91e01fbc243ccb446b8a96301b549dea
sbk283 <sbk283@naver.com>
2024-03-05 12:37:18 +0900

    front 작업중

--- src/test/java/com/project/Enovel/EnovelApplicationTests.java
+++ src/test/java/com/project/Enovel/EnovelApplicationTests.java
@@ -13,33 +13,5 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SbbApplicationTests {
-
-    @Autowired
-    private ProductRepository productRepository;
-
-    @Test
-    @Transactional
-    void testFindById() {
-        // Given
-        Long productId = 122L;
-        Optional<Product> optionalProduct = this.productRepository.findById(productId);
-
-        // Then
-        assertEquals(true, optionalProduct.isPresent());
-        optionalProduct.ifPresent(product -> assertEquals("샘플 상품 3", product.getProductName()));
-    }
-
-    @Test
-    @Transactional
-    void testFindBySubjectLike() {
-        // Given
-        String subject = "사과";
-        List<Product> productList = this.productRepository.findBySubjectLike(subject);
-
-        // Then
-        assertEquals(1, productList.size());
-        Product product = productList.get(0);
-        assertEquals("사과", product.getProductName());
-    }
+class EnovelApplicationTests {
 }
